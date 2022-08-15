28ab6bccfb63df4f99df55f000973ca97d4ca4c0
Teejay Dixon <tahjgdixon@gmail.com>
2022-08-15 15:38:30 -0700

    commit tree 2a6806ef1d45d689244ed28c9aa59a038b9d5c79

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+const removeName = document.getElementById('main');
+
+removeName.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.className = 'h1#victory'
+
+document.body.append(newHeader);
+
+const headerVariable = () => {
+  document.getElementById('victory')
+  return newHeader.textContent = 'Teejay is the champion'
+};
+
+headerVariable;
